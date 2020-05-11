6063f22ffc4925d521c84cc28e16e825f5fc9732
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-11 09:53:30 -0300

    fetch data from github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,64 +1,62 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
 const logo = require('../../assets/logo.svg');
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [searchInput, setSearchInput] = useState('');
+
+  async function handleAddRepository(
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    e.preventDefault();
+    const response = await api.get<Repository>(`repos/${searchInput}`);
+
+    setRepositories([...repositories, response.data]);
+    setSearchInput('');
+  }
+
   return (
     <>
       <img src={logo} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositótrio" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositótrio"
+          onChange={e => setSearchInput(e.target.value)}
+          value={searchInput}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/58868651?s=460&u=cfc57caf3987e37b236285f9dc032cb7a6fd4b0c&v=4"
-            alt="Rilton"
-          />
-          <div>
-            <strong>riltonfranzonee/fastfeet-api</strong>
-            <p>
-              Shipping company full control system - REST API made with Node.js
-            </p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/58868651?s=460&u=cfc57caf3987e37b236285f9dc032cb7a6fd4b0c&v=4"
-            alt="Rilton"
-          />
-          <div>
-            <strong>riltonfranzonee/fastfeet-api</strong>
-            <p>
-              Shipping company full control system - REST API made with Node.js
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/58868651?s=460&u=cfc57caf3987e37b236285f9dc032cb7a6fd4b0c&v=4"
-            alt="Rilton"
-          />
-          <div>
-            <strong>riltonfranzonee/fastfeet-api</strong>
-            <p>
-              Shipping company full control system - REST API made with Node.js
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
